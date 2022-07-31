a82b637c93a72e7fdbb9f7cccc8075829d3a092d
YuKongA <70465933+YuKongA@users.noreply.github.com>
2022-07-31 17:12:14 +0800

    rubens: Inherit from lineage

--- lineage_rubens.mk
+++ lineage_rubens.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common aosp stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from rubens device
 $(call inherit-product, device/xiaomi/rubens/device.mk)
