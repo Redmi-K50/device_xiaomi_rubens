45f90bc13c9b9a1573ca3cb01de23dd1e4d4e57c
wbs306 <wbs306@gmail.com>
2022-08-07 21:13:01 +0800

    rubens: power: Adapt to power V2 ndk_platform

--- power/Power.h
+++ power/Power.h
@@ -32,6 +32,11 @@ class Power : public BnPower {
     ndk::ScopedAStatus isModeSupported(Mode type, bool* _aidl_return) override;
     ndk::ScopedAStatus setBoost(Boost type, int32_t durationMs) override;
     ndk::ScopedAStatus isBoostSupported(Boost type, bool* _aidl_return) override;
+    ndk::ScopedAStatus createHintSession(int32_t tgid, int32_t uid,
+                                        const std::vector<int32_t>& threadIds,
+                                        int64_t durationNanos,
+                                        std::shared_ptr<IPowerHintSession>* _aidl_return) override;
+    ndk::ScopedAStatus getHintSessionPreferredRate(int64_t* outNanoseconds) override;
     ~Power();
 };
 
