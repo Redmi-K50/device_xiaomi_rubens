05926f6c1498d50d2d0d49ad35553fb9493d0838
YuKongA <70465933+YuKongA@users.noreply.github.com>
2022-08-19 14:48:07 +0800

    rubens: Remove useless

--- device.mk
+++ device.mk
@@ -201,8 +201,7 @@ PRODUCT_EXTRA_VNDK_VERSIONS := 31
 
 # Xiaomi Parts
 PRODUCT_PACKAGES += \
-    XiaomiParts \
-    gpu_optimizer.rc
+    XiaomiParts
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/rubens/rubens-vendor.mk)
